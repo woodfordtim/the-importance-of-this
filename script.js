fa39641e01ed6fef8f01821f5ea97f678edd8e36
Ubuntu <ubuntu@ip-172-31-39-123.ec2.internal>
2019-09-15 11:57:12 +0000

    completed lesson

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-$('button').mouseenter(function(){
-    $(this).removeClass('makeRed').addClass('makeBlue');
-});
-
-$('button').mouseleave(function(){
-    $(this).removeClass('makeBlue').addClass('makeRed');
-});
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        
+        /**
+         * when we click on an element that has
+         * a 'box' class, this event will be fired
+         */
+         var classNames = $(this).attr("class").split(" ");
+         var boxClass = classNames[0];
+         var className = classNames[1];
+         if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+             //if this object is already red, turn it black
+             $("." + className).css("background-color", "#000");
+         } else {
+             //if else turn all elemnents with a box class black
+             // and then change the color of all elements
+             // with the same class name as the clicked element
+             // to red
+             $("." + boxClass).css("background-color", "#000");
+             $("." + className).css("background-color", "red");
+         }
+    });
+})
